cdb8619b831ddc14ca4dd0b8e16c1ca82ddff3f1
librelois elois@ifee.fr <Administrateur@UTILISA-NPKASMV>
2018-08-22 10:55:31 +0200

    remove comment

--- m2i-java/src/Main.java
+++ m2i-java/src/Main.java
@@ -3,7 +3,6 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 public class Main {
 
 	public static void main(String[] args) {
-		//On crée notre collection
 	     ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<String>();
 	      	
 		ProductThread p = new ProductThread(queue);
