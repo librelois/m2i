1d40f2522e82dd4a3f435611502500d365aad1cd
librelois elois@ifee.fr <Administrateur@UTILISA-NPKASMV>
2018-08-22 11:13:50 +0200

    [fix] ConsumThread : treat empty queue case

--- m2i-java/src/ConsumThread.java
+++ m2i-java/src/ConsumThread.java
@@ -2,16 +2,18 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class ConsumThread extends Thread {
 	private ConcurrentLinkedQueue<String> queue;
-	
-	ConsumThread (ConcurrentLinkedQueue<String> queue) {
+
+	ConsumThread(ConcurrentLinkedQueue<String> queue) {
 		this.queue = queue;
 	}
-	
+
 	@Override
 	public void run() {
 		while (true) {
-			String msg = this.queue.poll();
-			System.out.println(msg);
+			if (this.queue.size() > 0) {
+				String msg = this.queue.poll();
+				System.out.println(msg);
+			}
 			try {
 				sleep(100);
 			} catch (InterruptedException e) {
