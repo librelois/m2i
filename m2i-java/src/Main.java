3051a1058e46aa2a3d9b6fa45b8cf9d914e55552
librelois <elois@ifee.fr>
2018-08-22 10:00:56 +0200

    add third thread

--- m2i-java/src/Main.java
+++ m2i-java/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 		ThreadA t1 = new ThreadA('+');
 		ThreadA t2 = new ThreadA('-');
+		ThreadA t3 = new ThreadA('*');
 		
 		t1.start();
 		t2.start();
+		t3.start();
 	}
 
 }
